7370879717321b9c8aad8e0a1252c43b5a673d77
LuigiBianco01 <92793421+LuigiBianco01@users.noreply.github.com>
2021-12-12 00:23:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 
-int main(){
-  return 0;
+using namespace std;
+
+#include "lib.h"
+
+
+int main() {
+    float d=0;
+    int i=0;
+    float b=0;
+    int c=0;
+    int n=0;
+    int h=0;
+    cin >> n;
+
+    float numeri [n];
+
+
+    while (i<n){
+        c=b;
+        cin >> b;
+        numeri[i]=b;
+        i++;
+    }
+
+
+    funzione(numeri, n);
+
+
+    cout << funzione(numeri, n) ;
+    return 0;
 }
