23a1b3d1d131b709b1810addd076bfbdbd2f3717
LuigiBianco01 <92793421+LuigiBianco01@users.noreply.github.com>
2021-12-12 00:26:16 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
 
+float funzione(float numeri[],int n){
+    int i=0;
+    float f=0;
+    float k=0;
+    float e=0;
+
+    while(i < n){
+        k=f;
+        f=numeri[i];
+        i++;
+        if(f>e){
+            e=f;
+        }
+    }
+    return e;
+}
